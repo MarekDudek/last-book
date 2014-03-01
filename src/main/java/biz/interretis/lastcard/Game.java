94364e688ca9b0b9d7fa2921eef4bf9ba2680742
Marek Dudek <marek.dudek@gmail.com>
2014-03-01 15:55:58 +0100

    All game history is preserved

--- src/main/java/biz/interretis/lastcard/Game.java
+++ src/main/java/biz/interretis/lastcard/Game.java
@@ -6,47 +6,56 @@ import java.util.Map;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 public class Game {
 
-    private Map<Player, Score> scores;
+    private Map<Player, Score> currentScores;
+    private Map<Player, List<Score>> history;
 
     public Game(final Player... players) {
-	scores = Maps.newLinkedHashMap();
+	currentScores = Maps.newLinkedHashMap();
+	history = Maps.newLinkedHashMap();
 	for (final Player player : players) {
-	    scores.put(player, Score.ZERO);
+	    currentScores.put(player, Score.ZERO);
+	    history.put(player, Lists.<Score> newLinkedList());
 	}
-	Preconditions.checkArgument(scores.size() >= 2, "Game needs at least two players");
-	Preconditions.checkArgument(players.length == scores.size(), "Players must be unique");
+	Preconditions.checkArgument(currentScores.size() >= 2, "Game needs at least two players");
+	Preconditions.checkArgument(players.length == currentScores.size(), "Players must be unique");
     }
 
     public List<Player> players() {
-	return ImmutableList.copyOf(scores.keySet());
+	return ImmutableList.copyOf(currentScores.keySet());
     }
 
     public Score score(final Player player) {
-	return scores.get(player);
+	return currentScores.get(player);
     }
 
     public void addPoints(final Player player, final Score points) {
-	final Score previous = scores.get(player);
+	final Score previous = currentScores.get(player);
 	final Score current = previous.increasedBy(points);
-	scores.put(player, current);
+	currentScores.put(player, current);
+	history.get(player).add(current);
     }
 
     public Player dealer() {
-	final Iterator<Player> players = scores.keySet().iterator();
+	final Iterator<Player> players = currentScores.keySet().iterator();
 	Player dealer = players.next();
-	Score highScore = scores.get(dealer);
+	Score highScore = currentScores.get(dealer);
 	while (players.hasNext()) {
 	    final Player player = players.next();
-	    final Score score = scores.get(player);
+	    final Score score = currentScores.get(player);
 	    if (score.compareTo(highScore) > 0) {
 		dealer = player;
 		highScore = score;
 	    }
 	}
 	return dealer;
+    }
+
+    public List<Score> getScoreHistory(final Player player) {
+	return ImmutableList.copyOf(history.get(player));
     }
 }
--- src/test/java/biz/interretis/lastcard/GameRecordTest.java
+++ src/test/java/biz/interretis/lastcard/GameRecordTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import com.google.common.collect.Lists;
+
 public class GameRecordTest {
 
     // given
@@ -75,9 +77,38 @@ public class GameRecordTest {
 	game.addPoints(SZNYC, score(18));
 	game.addPoints(JAM, score(16));
 
+	game.addPoints(MAREK, score(2));
+	game.addPoints(SZNYC, score(3));
+	game.addPoints(JAM, score(4));
+
 	// then
-	assertEquals(score(25), game.score(MAREK));
-	assertEquals(score(28), game.score(SZNYC));
-	assertEquals(score(31), game.score(JAM));
+	assertEquals(score(27), game.score(MAREK));
+	assertEquals(score(31), game.score(SZNYC));
+	assertEquals(score(35), game.score(JAM));
+    }
+
+    @Test
+    public void all_game_history_is_preserved()
+    {
+	// given
+	final Game game = new Game(MAREK, SZNYC, JAM);
+
+	// when
+	game.addPoints(MAREK, score(5));
+	game.addPoints(SZNYC, score(10));
+	game.addPoints(JAM, score(15));
+
+	game.addPoints(MAREK, score(20));
+	game.addPoints(SZNYC, score(18));
+	game.addPoints(JAM, score(16));
+
+	game.addPoints(MAREK, score(2));
+	game.addPoints(SZNYC, score(3));
+	game.addPoints(JAM, score(4));
+
+	// then
+	assertEquals(Lists.newArrayList(score(5), score(25), score(27)), game.getScoreHistory(MAREK));
+	assertEquals(Lists.newArrayList(score(10), score(28), score(31)), game.getScoreHistory(SZNYC));
+	assertEquals(Lists.newArrayList(score(15), score(31), score(35)), game.getScoreHistory(JAM));
     }
 }
